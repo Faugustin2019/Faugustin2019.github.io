910e0745a1f994e235889f565d53c4fe5890aeb8
Faugustin2019 <49660590+Faugustin2019@users.noreply.github.com>
2020-09-29 13:47:32 -0700

    Update post.js

--- post.js
+++ post.js
@@ -4,6 +4,8 @@
     "lang_code": "en",
     "url_pattern": "?"
   }],
+  custom_lang_switcher_html: "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a></div>",
+  custom_lang_switcher_container_id: "langlinks",
     "translated_segments": [
   {
     "lang_name": "Suomi",
