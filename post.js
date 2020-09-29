57d666d90cc1545909d9376b32e338d3c484b197
Faugustin2019 <49660590+Faugustin2019@users.noreply.github.com>
2020-09-29 14:03:53 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,17 +1,16 @@
- var linguJSON = {
-            "languages": [{
-    "lang_name": "English",
-    "lang_code": "en",
-    "url_pattern": "?"
-  }],
-  custom_lang_switcher_html: "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a></div>",
-  custom_lang_switcher_container_id: "langlinks",
-    "translated_segments": [
+var linguJSON = {
+    "languages": [{
+       "lang_name": "English",
+       "lang_code": "en",
+       "url_pattern": "?"
+  },
   {
     "lang_name": "Suomi",
     "lang_code":"es",
     "url_pattern": "?"
-  },
+  }
+ ],
+  "translated_segments": [
   {
     "source": "Legal",
     "target": "Oikeusperusta"
