ae7cfcd7ad5e9c14e0ae349db3b2bac64bf43057
Faugustin2019 <49660590+Faugustin2019@users.noreply.github.com>
2020-09-29 12:38:44 -0700

    Create post.js

---
+++ post.js
@@ -0,0 +1,78 @@
+ var linguJSON = {
+            "languages": [
+  {
+    "lang_name": "English",
+    "lang_code": "en",
+    "url_pattern": "/"
+  }],
+    "translated_segments": [
+  {
+    "lang_name": "Suomi",
+    "lang_code": "fi",
+    "url_pattern": "/"
+  },
+  {
+    "source": "Legal",
+    "target": "Oikeusperusta"
+  },
+  {
+    "source": "",
+    "target": ""
+  },
+  {
+    "source": "Contents",
+    "target": "Sisältö"
+  },
+  {
+    "source": "Important Safety Info",
+    "target": "Tärkeää turvallisuustietoa"
+  },
+  {
+    "source": "Patents",
+    "target": "Patentit"
+  },
+  {
+    "source": "Warranty",
+    "target": "Takuu"
+  },
+  {
+    "source": "",
+    "target": ""
+  },
+  {
+    "source": "TRADEMARK NOTICES",
+    "target": "TAVARAMERKKI-ILMOITUKSET"
+  },
+  {
+    "source": "Tandem Diabetes Care, Basal-IQ, Micro-Delivery, t:connect, t:flex, t:slim, and Touch Simplicity are trademarks of Tandem Diabetes Care, Inc., registered in the U.S. and other countries.",
+    "target": "Tandem Diabetes Care, Basal-IQ, Micro-Delivery, t:connect, t:flex, t:slim ja Touch Simplicity ovat Tandem Diabetes Care, Inc. -yhtiön tavaramerkkejä, jotka on rekisteröity Yhdysvalloissa ja muissa maissa."
+  },
+  {
+    "source": "AutoSoft, t:lock, t:simulator, t:slim G4, t:slim X2, TruSteel, and VariSoft are trademarks of Tandem Diabetes Care, Inc.",
+    "target": "AutoSoft, t:lock, t:simulator, t:slim G4, t:slim X2, TruSteel ja VariSoft ovat Tandem Diabetes Care, Inc. -yhtiön tavaramerkkejä."
+  },
+  {
+    "source": "The Bluetooth® wordmark and logos are registered trademarks owned by Bluetooth SIG, Inc. and any use of such marks by Tandem Diabetes Care, Inc. is under license.",
+    "target": "Sana Bluetooth® ja vastaavat logot ovat rekisteröityjä tavaramerkkejä, jotka omistaa Bluetooth SIG Inc., ja yhtiöllä Tandem Diabetes Care, Inc. on lisenssi niiden käyttöön."
+  },
+  {
+    "source": "Dexcom, Dexcom G5, Dexcom G6, and Dexcom Follow are registered trademarks of Dexcom, Inc. in the United States and/or other countries.",
+    "target": "Dexcom, Dexcom G5, Dexcom G6 ja Dexcom Follow ovat Dexcom, Inc:n rekisteröityjä tavaramerkkejä Yhdysvalloissa ja/tai muissa maissa."
+  },
+  {
+    "source": "Apple and the Apple logo are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc.",
+    "target": "Apple ja Apple-logo ovat Apple Inc. -yhtiön tavaramerkkejä, jotka on rekisteröity Yhdysvalloissa ja muissa maissa. App Store on Apple Inc. -yrityksen palvelumerkki."
+  },
+  {
+    "source": "Google Play and the Google Play logo are trademarks of Google LLC.",
+    "target": "Google Play ja Google Play -logo ovat Google LLC:n tavaramerkkejä."
+  },
+  {
+    "source": "",
+    "target": ""
+  },
+  {
+    "source": "All other third-party marks are the property of their respective owners.",
+    "target": "Kaikki muut kolmannen osapuolen merkit ovat omistajiensa omaisuutta."
+  }
+]
