ba75adedfbe2eebc790157b509ec7e5d46fb44d7
Faugustin2019 <49660590+Faugustin2019@users.noreply.github.com>
2020-09-30 16:57:21 -0700

    Update post.js

--- post.js
+++ post.js
@@ -1,245 +1,111 @@
-
 var linguJSON = {
-   "languages": [
-  {
-    "lang_name": "English",
-    "lang_code": "en",
-    "url_pattern": "?"
+    "languages": [{
+       "lang_name": "English",
+       "lang_code": "en",
+       "url_pattern": "?"
   },
   {
     "lang_name": "Suomi",
-    "lang_code": "es",
+    "lang_code":"es",
     "url_pattern": "?"
   }
-],
-   "translated_segments": [
+ ],
+  "translated_segments": [
   {
-    "source": "Control-IQ Technology Software Update",
-    "target": "Control-IQ™-teknologiaohjelmistopäivitys"
+    "source": "IMPORTANT SAFETY INFORMATION - TANDEM DEVICE UPDATER",
+    "target": "Tärkeää turvallisuustietoa - TANDEM DEVICE UPDATER"
+  },
+  {
+    "source": "Intended Use",
+    "target": "Käyttötarkoitus"
+  },
+  {
+    "source": "The Tandem Device Updater is intended for use by individuals with diabetes mellitus who use Tandem Diabetes Care® insulin pumps, their caregivers, and their healthcare professionals in home and clinical settings. The Tandem Device Updater provides Tandem insulin pump users the ability to update their device software remotely using a computer with internet access.",
+    "target": "Tandem-laitepäivitysohjelma on tarkoitettu diabetes mellitusta sairastaville potilaille, jotka käyttävät Tandem Diabetes Care®:n insuliinipumppuja, sekä heidän hoitajilleen ja terveydenhuollon ammattilaisille kotona ja kliinisessä ympäristössä. Tandem-laitepäivitysohjelma antaa Tandem-insuliinipumpun käyttäjille mahdollisuuden päivittää laiteohjelmisto etänä käyttäen tietokonetta ja Internet-yhteyttä."
+  },
+  {
+    "source": "CAUTION: Federal (USA) law restricts Tandem Diabetes Care insulin pumps to sale by or on the order of a physician.",
+    "target": "HUOMIO: Yhdysvaltain liittovaltion laki rajoittaa Tandem Diabetes Care -insuliinipumppujen myynnin vain lääkäreille tai lääkärin määräyksestä."
   },
   {
     "source": "",
     "target": ""
   },
   {
-    "source": "Tandem carefully develops software updates with your safety and satisfaction in mind. Please review the instructions and take time to understand the new features before beginning your update.",
-    "target": "Tandem kehittää ohjelmistopäivityksiä huolellisesti pitäen mielessään turvallisuutesi ja tyytyväisyytesi. Tarkista ohjeet ja käytä aikaa uusien toimintojen ymmärtämiseen ennen päivityksen aloittamista."
+    "source": "WARNINGS AND PRECAUTIONS",
+    "target": "Varoitukset ja varotoimet"
+  },
+  {
+    "source": "WARNINGS",
+    "target": "VAROITUSS"
+  },
+  {
+    "source": "DO NOT update your Tandem insulin pump before reading the User Guide. Incorrect use of the Tandem Device Updater or failure to follow the appropriate instructions can lead to an inoperable pump. If you have questions or need further clarification on the Tandem Device Updater or pump use, contact Customer Technical Support.",
+    "target": "ÄLÄ päivitä Tandem-insuliinipumppuasi ennen kuin olet lukenut käyttöoppaan. Tandem-laitepäivitysohjelman väärinkäyttö tai ohjeiden noudattamatta jättäminen voi johtaa pumpun toimimattomuuteen. Jos sinulla on kysyttävää tai tarvitset lisäselvennystä Tandem-laitepäivitysohjelmasta tai pumpun käytöstä, ota yhteyttä tekniseen asiakastukeen."
+  },
+  {
+    "source": "DO NOT update your pump while your infusion set is connected to your body. Always ensure that the infusion set is disconnected from your body before updating your pump software.",
+    "target": "ÄLÄ päivitä pumppua infuusiosetin ollessa kytkettynä kehoosi. Varmista aina, että infuusiosetti on irrotettu kehostasi ennen pumpun ohjelmiston päivittämistä."
+  },
+  {
+    "source": "DO suspend all pump insulin delivery prior to using the Tandem Device Updater.",
+    "target": "PYSÄYTÄ kaikki pumpun insuliinin annostelu ennen Tandem-laitepäivitysohjelman käyttämistä."
+  },
+  {
+    "source": "DO check your blood glucose (BG) prior to suspending delivery and be sure to treat high or low BG levels as directed by your healthcare professional prior to update.",
+    "target": "TARKISTA verensokerisi (VS) ennen annostelun pysäyttämistä ja huolehdi siitä, että hoidat korkean tai matalan VS-tason terveydenhuollon ammattilaisen ohjeiden mukaan ennen päivityksen tekemistä."
+  },
+  {
+    "source": "DO be prepared to inject insulin with an alternative method in case you encounter any issues while updating your pump. Failure to have an alternative method of insulin delivery can lead to very high blood glucose or Diabetic Ketoacidosis (DKA).",
+    "target": "VALMISTAUDU injektoimaan insuliinia vaihtoehtoisella tavalla, jos pumpun päivityksen aikana ilmenee ongelmia. Vaihtoehtoisen insuliinin annostelutavan puuttuminen voi johtaa erittäin korkeaan verensokeriin tai diabeettiseen ketoasidoosiin (DKA)."
+  },
+  {
+    "source": "DO complete the training appropriate to your device and feature set before starting to use the updated software. Failure to complete necessary training could result in serious injury or death.",
+    "target": "SUORITA laitettasi ja toimintojasi koskeva koulutus ennen kuin alat käyttää päivitettyä ohjelmistoa. Jos et suorita tarvittavaa koulutusta, seurauksena voi olla vakava loukkaantuminen tai kuolema."
+  },
+  {
+    "source": "DO NOT rely on the Insulin On Board (IOB) displayed on your pump after an update until your prior IOB has been depleted. Your IOB will be reset to zero during the update process. Since the calculated bolus amount relies on IOB, it could prompt you to deliver more insulin than needed and result in hypoglycemia. Consult with your healthcare professional for how long you need to wait after an update before you can rely on the IOB calculation.",
+    "target": "ÄLÄ luota pumpussa näkyvään aktiiviseen insuliiniin (IOB) päivityksen jälkeen ennen kuin aiempi IOB:si on kulutettu loppuun. IOB:si nollataan päivityksen yhteydessä. Koska laskettu bolusmäärä riippuu IOB:sta, saatat saada kehotteen annostella tarvitsemaasi enemmän insuliinia, jolloin seurauksena voi olla hypoglykemia. Kysy terveydenhuollon ammattilaiselta, kuinka kauan sinun tarvitsee päivityksen jälkeen odottaa ennen kuin IOB-laskelmat ovat taas luotettavia."
+  },
+  {
+    "source": "DO confirm your pump’s personal settings, date, and time are correct after the update. Incorrect settings can result in over delivery or under delivery of insulin. Consult with your healthcare professional as needed to establish appropriate settings.",
+    "target": "VAHVISTA, että pumppusi henkilökohtaiset asetukset, päivämäärä ja kellonaika ovat oikeat päivityksen jälkeen. Virheelliset asetukset voivat johtaa siihen, että insuliinia annostellaan liikaa tai liian vähän. Ota tarvittaessa yhteyttä terveydenhuollon ammattilaiseen sopivien asetusten määrittämiseksi."
   },
   {
     "source": "",
     "target": ""
   },
   {
-    "source": "What's Included",
-    "target": "Sisältö"
+    "source": "PRECAUTIONS",
+    "target": "VAROTOIMIS"
   },
   {
-    "source": "This update adds Control-IQ™ technology to your pump. Control-IQ technology is designed to help increase time in range (3.9-10 mmol/L)* using Dexcom G6 continuous glucose monitoring (CGM)† values to predict glucose levels 30 minutes ahead and adjust insulin accordingly.",
-    "target": "Tämä päivitys lisää Control-IQ ™-teknologian pumppuusi. Control-IQ-teknologia on suunniteltu auttamaan pysymistä alueella (3,9 ̶ 10 mmol/l)* käyttämällä Dexcom G6:n jatkuvan glukoosimonitoroinnin (CGM)† arvoja glukoositasojen ennakointiin 30 minuuttia eteenpäin ja insuliinin säätämiseen vastaavasti."
+    "source": "DO NOT shut down your computer or allow it to go into “hibernate”, “sleep” or “standby” mode during an update. If you are using a laptop, do not close the laptop. Doing so could interrupt the update, and your pump may not function.",
+    "target": "ÄLÄ sammuta tietokonetta äläkä anna sen siirtyä horros-, lepo- tai valmiustilaan päivityksen aikana. Jos käytät kannettavaa tietokonetta, älä sulje sitä. Päivitys voi keskeytyä, eikä pumppusi välttämättä toimi."
   },
   {
-    "source": "LEARN MORE",
-    "target": "LUE LISÄÄ"
+    "source": "DO NOT disconnect from the internet during an update. Doing so could interrupt the update, and your pump may not function.",
+    "target": "ÄLÄ katkaise Internet-yhteyttä päivityksen aikana. Päivitys voi keskeytyä, eikä pumppusi välttämättä toimi."
   },
   {
-    "source": "",
-    "target": ""
+    "source": "DO NOT disconnect your pump from your computer during an update unless instructed to do so. Doing so could interrupt the update, and your pump may not function.",
+    "target": "ÄLÄ irrota pumppua tietokoneesta päivityksen aikana, ellei niin ohjeisteta. Päivitys voi keskeytyä, eikä pumppusi välttämättä toimi."
   },
   {
-    "source": "Update Eligibility",
-    "target": "Päivitä kelpoisuus"
-  },
-  {
-    "source": "Contact your local Customer Technical Support to determine if your pump is eligible for an update.",
-    "target": "Ota yhteys paikalliseen tekniseen asiakastukeen ja selvitä, voidaanko pumppusi päivittää."
+    "source": "DO NOT rely on the Max Hourly Bolus Alert for 60 minutes following an update. Your Max Hourly Bolus will be reset to zero during the update process.",
+    "target": "ÄLÄ luota Maks.bolus tunnissa -varoitukseen 60 minuuttiin päivityksen jälkeen. Maks.bolus tunnissa nollataan päivityksen yhteydessä."
   },
   {
     "source": "",
     "target": ""
   },
   {
-    "source": "This update may require a prescription and you will need to complete training before you can receive the unique Update ID needed to update your t:slim X2™ insulin pump with Control-IQ technology. If you completed the training process and did not receive your code or are unsure if your pump is eligible for this update, please contact your local Customer Technical Support.",
-    "target": "Tämä päivitys saattaa vaatia tilauksen ja sinun on suoritettava koulutus, ennen kuin voit saada yksilöllisen päivitystunnuksen ja päivittää t:slim X2™ -insuliinipumppusi sisältämään Control-IQ-teknologian. Jos suoritit koulutusprosessin etkä saanut tunnusta tai et ole varma, onko pumppusi kelvollinen tähän päivitykseen, ota yhteyttä paikalliseen tekniseen asiakastukeen."
+    "source": "Important User Information",
+    "target": "Tärkeää käyttäjätietoa"
   },
   {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "Update Your Pump",
-    "target": "Päivitä pumppusi"
-  },
-  {
-    "source": "Here are the three simple steps to update the software on your t:slim X2 insulin pump:",
-    "target": "Vain kolme yksinkertaista vaihetta t:slim X2 -insuliinipumpun ohjelmiston päivittämiseen:"
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "BEFORE YOU BEGIN",
-    "target": "ENNEN KUIN ALOITAT"
-  },
-  {
-    "source": "Control-IQ technology only works with Dexcom G6 CGM. Your t:slim X2 insulin pump will no longer be compatible with Dexcom G5 Mobile CGM following this update. If you have G5 supplies you plan to use, we recommend postponing this update until you are ready to switch to the new Dexcom G6 CGM System.",
-    "target": "Control-IQ-teknologia toimii vain Dexcom G6 CGM:n kanssa. t:slim X2 -insuliinipumppu ei ole enää yhteensopiva Dexcom G5 Mobile CGM:n kanssa tämän päivityksen jälkeen. Jos sinulla on G5-tarvikkeita, joita aiot käyttää, suosittelemme tämän päivityksen lykkäämistä, kunnes olet valmis vaihtamaan uuteen Dexcom G6 CGM -järjestelmään."
-  },
-  {
-    "source": "Please be aware that after you update your t:slim X2 insulin pump to Control-IQ technology, you cannot revert from Control-IQ technology to the previous software version that was on your pump.",
-    "target": "Huomaa, että kun olet päivittänyt t:slim X2 -insuliinipumpun Control-IQ-teknologiaan, et voi palata Control-IQ-teknologiasta pumpun aiempaan ohjelmistoversioon."
-  },
-  {
-    "source": "Please consult the Tandem Device Updater User Guide and have the following on hand:",
-    "target": "Tutustu Tandem-laitepäivitysohjelman käyttöoppaaseen ja varmista, että sinulla on:"
-  },
-  {
-    "source": "Your Update ID. If you cannot locate your Update ID, contact your local Customer Technical Support.",
-    "target": "Päivitystunnuksesi. Jos et löydä päivitystunnustasi, ota yhteyttä paikalliseen tekniseen asiakastukeen."
-  },
-  {
-    "source": "A backup insulin delivery option, such as a syringe and fast-acting insulin, in case there are any issues updating your insulin pump. These items should be part of your Emergency Kit and should be kept with you at all times.",
-    "target": "Varajärjestelmä insuliinin annosteluun, kuten ruisku ja nopeavaikutteista insuliinia, mikäli insuliinipumppusi päivityksessä esiintyisi ongelmia. Näitä sinun tulisi pitää ensiapupakkauksessa ja aina mukanasi."
-  },
-  {
-    "source": "Your blood glucose meter so that you can check your BG prior to starting the update.",
-    "target": "Verensokerimittarisi, jotta voit mitata VS:n ennen päivityksen aloittamista."
-  },
-  {
-    "source": "A convenient time to stop all insulin delivery, detach your infusion set, and remove your cartridge. We recommend performing the update when you are ready to change your cartridge.",
-    "target": "Varaa sopiva aika, jolloin voit pysäyttää kaiken insuliinin annostelun sekä irrottaa infuusiosettisi ja säiliösi. Suosittelemme suorittamaan päivityksen, kun olet valmis vaihtamaan säiliösi."
-  },
-  {
-    "source": "A computer with an internet connection. If you are using a laptop, make sure that it remains plugged in to a wall outlet during the half hour it will take to complete the software update.",
-    "target": "Tietokone ja Internet-yhteys. Jos käytät kannettavaa tietokonetta, varmista, että se on kytkettynä pistorasiaan sen puolen tunnin ajan, jonka ohjelmistopäivityksen suorittaminen kestää."
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "INSTALL TANDEM DEVICE UPDATER DOWNLOAD",
-    "target": "ASENNA LADATTU TANDEM-LAITEPÄIVITYSOHJELMA"
-  },
-  {
-    "source": "UPDATE YOUR DEVICE",
-    "target": "PÄIVITÄ LAITTEESI"
-  },
-  {
-    "source": "The Tandem Device Updater should automatically start after you install it on your computer. Follow the prompts to update your t:slim X2 insulin pump. The process should take less than 10 minutes to complete.",
-    "target": "Tandem-laitepäivitysohjelman tulisi käynnistyä automaattisesti, kun olet asentanut sen tietokoneellesi. Päivitä t:slim X2 -insuliinipumppu seuraamalla ohjeita. Prosessin suorittaminen kestää alle 10 minuuttia."
-  },
-  {
-    "source": "If the Updater encounters a condition that will not allow you to continue, you will see an error message on your computer screen. Follow the on-screen instructions to resolve the error and continue with your update, or contact your local Tandem Customer Technical Support. If you have trouble connecting your pump or starting an update, reference the Troubleshooting section of the Tandem Device Updater User Guide.",
-    "target": "Jos päivitysohjelma havaitsee tilan, jossa päivitystä ei voi jatkaa, tietokoneen näytölle ilmestyy virheviesti. Noudata näytöllä olevia ohjeita virhetilan ratkaisemiseksi ja päivityksen jatkamiseksi tai ota yhteyttä Tandemin tekniseen asiakaspalveluun. Jos sinulla on vaikeuksia pumpun kytkemisessä tai päivityksen käynnistämisessä, tutustu Tandem-laitepäivitysohjelmiston käyttöoppaan Vianmääritys-osioon."
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "System Requirements",
-    "target": "Laitteistovaatimukset"
-  },
-  {
-    "source": "A computer and internet connection will be required to update your pump. Please take a minute to review the following technical requirements. In order to install Tandem’s device updater software, you will need administrator rights on your computer.",
-    "target": "Pumpun päivittämiseen vaaditaan tietokone ja Internet-yhteys. Käytä hetki seuraavien teknisten vaatimusten tarkistamiseen. Tarvitset tietokoneesi järjestelmänvalvojan oikeudet Tandemin laitepäivitysohjelman asentamiseksi."
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "SUPPORTED DEVICES",
-    "target": "TUETUT LAITTEET"
-  },
-  {
-    "source": "t:slim X2™ insulin pump",
-    "target": "t:slim X2 -insuliinipumppu"
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "SUPPORTED OPERATING SYSTEMS",
-    "target": "TUETUT KÄYTTÖJÄRJESTELMÄT"
-  },
-  {
-    "source": "Microsoft Windows 10 (64 bit)",
-    "target": "Microsoft Windows 10 (64-bittinen)"
-  },
-  {
-    "source": "Microsoft Windows 8.1 (64 bit)",
-    "target": "Microsoft Windows 8.1 (64-bittinen)"
-  },
-  {
-    "source": "Microsoft Windows 7 (64 bit)",
-    "target": "Microsoft Windows 7 (64-bittinen)"
-  },
-  {
-    "source": "Mac OS Catalina (v10.15)",
-    "target": "Mac OS Catalina (v10.15)"
-  },
-  {
-    "source": "Mac OS Mojave (v10.14)",
-    "target": "Mac OS Mojave (v10.14)"
-  },
-  {
-    "source": "Mac OS High Sierra (v10.13)",
-    "target": "Mac OS High Sierra (v10.13)"
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "MINIMUM COMPUTER REQUIREMENTS",
-    "target": "TIETOKONEEN VÄHIMMÄISVAATIMUKSET"
-  },
-  {
-    "source": "1 GB RAM",
-    "target": "1 Gt RAM"
-  },
-  {
-    "source": "1 GB of available disk space",
-    "target": "1 Gt vapaata levytilaa"
-  },
-  {
-    "source": "x86 compatible 1.6 GHz processor",
-    "target": "x86-yhteensopiva 1,6 GHz:n prosessori"
-  },
-  {
-    "source": "USB 1.1 port or later",
-    "target": "USB 1.1 -portti tai uudempi"
-  },
-  {
-    "source": "Screen display capable of at least 1024 by 768 resolution",
-    "target": "Näyttö, jonka resoluutio on vähintään 1024 x 768"
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "Support Resources",
-    "target": "Tukiresurssit"
-  },
-  {
-    "source": "TANDEM DEVICE UPDATER USER GUIDE",
-    "target": "TANDEM-LAITEPÄIVITYSOHJELMAN KÄYTTÖOPAS"
-  },
-  {
-    "source": "",
-    "target": ""
-  },
-  {
-    "source": "* As measured by CGM.",
-    "target": "* CGM:n mittaamana."
-  },
-  {
-    "source": "† Dexcom CGM sold separately. Transmitter can only be paired with one medical device (either a Dexcom receiver or t:slim X2 insulin pump) and one consumer device (phone or tablet) at the same time.",
-    "target": "* Dexcom CGM myydään erikseen. Lähettimellä voidaan muodostaa pari vain yhden lääkinnällisen laitteen (joko Dexcom-vastaanottimen tai t:slim X2 -insuliinipumpun) ja yhden kuluttajalaitteen (puhelimen tai tabletin) kanssa samanaikaisesti."
+    "source": "Thoroughly review all product instructions for warnings and other important user information before using the Tandem Device Updater. Incorrect use may result in an inoperable pump. For contact information in your region see the back cover of this user guide.",
+    "target": "Lue tuotteen käyttöohjeista huolellisesti kaikki varoitukset ja muut tärkeät käyttäjätiedot ennen Tandem-laitepäivitysohjelman käyttämistä. Virheellinen käyttö voi johtaa pumpun toimimattomuuteen."
   }
 ]
  };
